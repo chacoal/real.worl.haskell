023ef04475fb737402e06ab9ce35a838168f138d
Takeru Sato <midium.size@gmail.com>
2014-08-26 01:50:58 +0000

    Add dlts.hs

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -25,3 +25,33 @@ myFoldl f z xs = foldr step id xs z
             == foldl (+) (((0 + 1) + 2) + 3)    []
             ==           (((0 + 1) + 2) + 3)
 -}
+
+fold' _    zero []     = zero
+fold' step zero (x:xs) =
+  let new = step zero x
+  in new `seq` fold' step new xs
+
+
+hiddenInside x y = someFunc (x `seq` y)
+
+hiddenByLet x y z = let a = x `seq` someFunc y
+                    in anotherFunc a z
+
+
+onTheOutside x y = x `seq` someFunc y
+
+chained x y z = x `seq` y `seq` someFunc z
+
+badExpression step zero (x:xs) =
+  seq (step zero x)
+      (badExpression step (step zero x) xs)
+
+
+someFunc a = undefined
+anotherFunc a z = undefined
+
+
+strictPair (a,b) = a `seq` (a, b)
+
+strictList (x:xs) = x `seq` x : strictList xs
+strictList []     = []
